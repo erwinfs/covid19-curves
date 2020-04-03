52be7ac6c6435226fa94b980ca3c09d9b2a3e2bf
Erwin Frank-Schultz <erwin.frank-schultz@uk.ibm.com>
2020-04-03 11:02:17 +0100

    Daily run

--- src/download-uk-data.py
+++ src/download-uk-data.py
@@ -1,7 +1,7 @@
 import wget
 import os
 
-filePath = 'DailyConfirmedCases.xlsx'
+filePath = './data/DailyConfirmedCases.xlsx'
 url = "https://www.arcgis.com/sharing/rest/content/items/e5fd11150d274bebaaf8fe2a7a2bda11/data"
 
 if os.path.exists(filePath):
